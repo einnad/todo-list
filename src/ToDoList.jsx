4669c216d9c70d2f40a06a898881d252f4b7b7f9
einnad <146109295+einnad@users.noreply.github.com>
2024-04-09 19:32:39 +0100

    Init list structure

---
+++ src/ToDoList.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([
+    "Go for Run",
+    "French prac",
+    "Code",
+    "Check discord",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInput(e) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() != "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <div className="to-do-list">
+      <h1>To-Do List</h1>
+
+      <div>
+        <input
+          type="text"
+          onChange={handleInput}
+          placeholder="Enter a task"
+          value={newTask}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add Task
+        </button>
+      </div>
+
+      <ul>
+        {tasks.map((t, i) => (
+          <li key={i}>
+            <p>{t}</p>
+            <button className="remove-button" onClick={() => deleteTask(i)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
